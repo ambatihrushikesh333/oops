6a5f45296d2791fb80d51bb4415c787e704ea120
ADMIN <ADMIN@H-RUSHI-KESH>
2026-05-21 11:10:43 +0530

    Add project files.

---
+++ oops/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oops
+{
+    class student
+    {
+        int sid;
+        string name;
+        public student(int id, string name)
+        {
+            this.sid = id;
+            this.name = name;
+        }
+        public void display()
+        {
+            Console.WriteLine("Student id: " + sid);
+            Console.WriteLine("Student name: " + name);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            student s1 = new student(101, "John");
+            s1.display();
+        }
+    }
+}
