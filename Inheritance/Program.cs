17b4c8da85f023ad150920a3c26be40064212955
ADMIN <ADMIN@H-RUSHI-KESH>
2026-05-25 15:25:09 +0530

    git

--- Inheritance/Program.cs
+++ Inheritance/Program.cs
@@ -6,10 +6,50 @@ using System.Threading.Tasks;
 
 namespace Inheritance
 {
+    class branch
+    {
+        int bid;
+        string bname;
+        string baddress;
+        public branch(int bid,string bname, string baddress)
+        {
+            this.bid = bid;
+            this.bname = bname;
+            this.baddress = baddress;
+        }
+        public void bdisplay()
+        {
+            Console.WriteLine("Branch id is " + bid);
+            Console.WriteLine("Branch name is " + bname);
+            Console.WriteLine("Branch address is " + baddress);
+        }
+    }
+    class student : branch
+    {
+        int sid;
+        string sname;
+        string scollege;
+
+        public student(int sid, string sname, string scollege):base (111,"bbb","Amerapet")
+        {
+            this.sid = sid;
+            this.sname = sname;
+            this.scollege = scollege;
+        }
+        public void sdisplay()
+        {
+            Console.WriteLine("Student id is " + sid);
+            Console.WriteLine("Student name is " + sname);
+            Console.WriteLine("Student college is " + scollege);
+            base.bdisplay();
+        }
+    }    
     internal class Program
     {
         static void Main(string[] args)
         {
+            student s = new student(1, "John", "IIIT");
+            s.sdisplay();
         }
     }
 }
